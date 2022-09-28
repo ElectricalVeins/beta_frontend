2bfd80fafcf8dab958ae04e2723fa3943f9dc1f2
ElectricalVeins <maxmost512@gmail.com>
2022-09-29 01:56:07 +0300

    Change ModalForm

--- src/components/forms/ModalForm.jsx
+++ src/components/forms/ModalForm.jsx
@@ -1,4 +1,5 @@
 import { Form, Modal } from 'antd';
+import { useState } from 'react';
 import { notificateError } from '../../utils/notification';
 
 export const ModalForm = ({
@@ -8,35 +9,39 @@ export const ModalForm = ({
   onSuccess,
 }) => {
   const [form] = Form.useForm();
+  const [submitting, setSubmitting] = useState(false);
   const [open, setOpen] = controls;
 
   const closeModal = () => setOpen(false);
 
-  const onCreate = async (values) => {
-    console.log('Received values of form: ', values);
-    await onSuccess(values);
-    closeModal();
-  };
   const onCancel = () => closeModal();
 
   const formSubmit = async () => {
+    setSubmitting(true);
     try {
       const data = await form.validateFields();
-      await onCreate(data);
+      await onSuccess(data);
+      closeModal();
       form.resetFields();
     } catch (error) {
-      console.log('Validate Failed:', error);
-      notificateError(error);
+      if (error.message) {
+        notificateError(error);
+      }
+    } finally {
+      setSubmitting(false);
     }
   };
   return (
     <Modal
       open={open}
       title={name}
-      okText={name}
+      okText={submitting ? 'Submitting' : name}
       cancelText="Cancel"
       onCancel={onCancel}
       onOk={formSubmit}
+      confirmLoading={submitting}
+      okButtonProps={{ disabled: submitting }}
+      maskStyle={submitting ? { backgroundColor: 'rgba(0,0,0,0.6)' } : null}
     >
       <FormComponent form={form} />
     </Modal>
